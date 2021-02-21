fdc5d2f1fe45d5e2d3bcd01cf95ff4071eabb932
dadcp88 <34496381+dadcp88@users.noreply.github.com>
2021-02-21 00:39:41 -0500

    Added Contact List Select procedure to read database

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from mysqlconnection import init_database
+from mysqlconnection import init_database, contact_list_select
 
 # This is a sample Python script.
 
 
 if __name__ == '__main__':
-    prompt_db = input("Como se llama la base de datos?")
-init_database(prompt_db)
+
+    print(f"Bienvenido a la lista de contacto, que quieres hacer?")
+    user_choice = input(f'1 - Inicializar BD\n'
+                        f'2 - Buscar los contactos guardados\n'
+                        )
+
+    if user_choice == '1':
+        prompt_db = input("Como se llama la base de datos?")
+        init_database(prompt_db)
+        print(f'base de datos creada')
+
+    if user_choice == '2':
+        contact_list_select()
+        print(f'Hola')
--- mysqlconnection.py
+++ mysqlconnection.py
@@ -2,21 +2,43 @@ from mysql.connector import connect, Error
 
 
 def init_database(db_name):
+    db_name = 'CONTACT_LIST'
     try:
         with connect(
                 host="localhost",
                 user="root",
                 password="12345678"
         ) as connection:
-            print(connection)
+            # print(connection)
             create_db_query = "CREATE DATABASE " + db_name + ";" \
                                                              "USE " + db_name + ";" \
                                                                                 "CREATE TABLE PERSON (" \
+                                                                                "idPerson varchar(40)," \
                                                                                 "Name varchar(40)," \
-                                                                                "Address varchar(40)," \
-                                                                                "phone varchar(40));"
+                                                                                "email varchar(40)," \
+                                                                                "address varchar(40)," \
+                                                                                "phone_number varchar(40));"
             cursor = connection.cursor()
-            cursor.execute(create_db_query)
+            cursor.execute(create_db_query, multi=True)
+            cursor.close()
 
+    except Error as e:
+        print(e)
+
+def contact_list_select():
+    db_name = 'CONTACT_LIST'
+    try:
+        with connect(
+                host="localhost",
+                user="root",
+                password="12345678",
+                database='CONTACT_LIST'
+        ) as connection:
+            select_movies_query = "SELECT * FROM Person;"
+            with connection.cursor() as cursor:
+                cursor.execute(select_movies_query)
+                result = cursor.fetchall()
+            for row in result:
+                print(row)
     except Error as e:
         print(e)
